41189a893f55f8d8c5e16023fc5cb5c70849b3dd
sinthu <sksinthu15@gmail.com>
2022-07-09 03:15:53 -0400

    crudapp1

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-//requiring all  dependencies
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-
+const db             = require('./config/db');
 const app            = express();
 const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
-
-
+  const dbo=database.db("note-api")
+  require('./app/routes')(app, dbo);
+  
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
